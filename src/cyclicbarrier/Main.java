f1753038dee618e217701839c8d2f71b7bda8c3a
Shyamsunder Yadav <y.shyam287@gmail.com>
2021-09-11 11:16:18 +0530

    cyclic barrier implementation

---
+++ src/cyclicbarrier/Main.java
@@ -0,0 +1,81 @@
+package cyclicbarrier;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        CyclicBarrier barrier = new CyclicBarrier(4);
+
+        Runnable runnable = getRunnable(barrier);
+
+        Callable<String> callable = getCallable(barrier);
+
+        ExecutorService ec = Executors.newFixedThreadPool(4);
+        List<Future<String>> futures = new ArrayList<>();
+
+        for (int i=0; i<2; i++) {
+            futures.add((Future<String>) ec.submit(runnable));
+            futures.add(ec.submit(callable));
+        }
+
+        printFutures(futures);
+
+        shutdownThreads(ec);
+
+    }
+
+    private static void printFutures(List<Future<String>> futures) {
+        for(Future<String> future: futures) {
+            try {
+                System.out.println("output future "+ future.get());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static Callable<String> getCallable(CyclicBarrier barrier) {
+
+        return () -> {
+            System.out.println("awwating for barrier to open in callable task " + Thread.currentThread().getName());
+            barrier.await();
+            System.out.println("barrier opened " + Thread.currentThread().getName());
+            return "completed " + Thread.currentThread().getName();
+        };
+    }
+
+    private static Runnable getRunnable(CyclicBarrier barrier) {
+        return () -> {
+
+            System.out.println("awaiting for barrier in runnable task " + Thread.currentThread().getName());
+            try {
+                barrier.await(1, TimeUnit.MILLISECONDS); // added to simulate BrokenBarrierException
+                System.out.println("barrier opened " + Thread.currentThread().getName());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException | TimeoutException e) {
+                e.printStackTrace();
+            }
+        };
+    }
+
+    private static void shutdownThreads(ExecutorService ec) {
+
+        ec.shutdown();
+        try {
+            ec.awaitTermination(5, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            if(!ec.isShutdown()) {
+                ec.shutdownNow();
+            }
+        }
+    }
+}
--- src/producerconsumer/blockingqueue/Main.java
+++ src/producerconsumer/blockingqueue/Main.java
@@ -5,8 +5,6 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
 
